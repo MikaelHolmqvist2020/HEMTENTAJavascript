ef960e4ccdc7ea7bcba1246e9245d20179caa60d
MikaelHolmqvist2020 <holmqvistmikael2020@gmail.com>
2021-02-27 13:40:13 +0100

    first commit

---
+++ src/view/uppgift2/Uppgift2.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const Uppgift2 = () => {
+	
+	const [name, setName] = useState()
+	
+	return (
+		
+		<div>
+			<h1>Uppgift 2</h1>
+			<h2>{name}</h2>
+			<input placeholder="Write My State"
+			value={name} onChange={x => setName(x.target.value)}/>
+		</div>
+	)
+}
+
+/*
+
+INSTRUKTIONER FÖR UPPGIFT 2:
+Denna vy skall använda sig av useState hook.
+
+	1. Importera samt instansiera en ny useState.
+	Du kan döpa variablen samt funktionsnamnet i din useState deklaration efter fri vilja. (valfritt)
+
+	2. Initialvärdet skall vara en valfri sträng
+
+	3. Värdet av ditt state skall synas i <h2> taggen på rad 7
+
+	4. Det värde som matas in i <input> taggen på rad 8 skall uppdateras till att
+	   vara det värde som finns i ditt state
+
+ */
